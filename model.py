966dde698d39223be850da1caf2be7e1caf35640
Ed Muthiah <ed.muthiah@gmail.com>
2020-10-08 22:32:06 +1100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import torchvision
+from torchvision import *
+
+class BaseModel(nn.Module):
+    def __init__(self, args):
+        super(BaseModel, self).__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.args = args
+        self.class_num = 10
+
+        ### initialize the BaseModel --------------------------------
+        #
+        #
+        self.net = models.resnet18(pretrained=True)
+        # self.net = self.net.cuda() if self.device else self.net
+        self.dcn_model = nn.Sequential(*list(resnet18.children())[:5])
+        self.w_dyn = nn.Sequential(
+            nn.Linear(4096,576),
+            nn.Tanh()
+            )
+        self.dc = nn.Sequential(
+            nn.Conv2d(64,1,3,stride=2,padding=1)
+            )
+        self.w_cls = nn.Sequential(
+            nn.Linear(64,10),
+            )
+        #
+        #
+        ### ---------------------------------------------------------
+
+    def forward(self, imgs, with_dyn=True):
+        if with_dyn:
+			''' with dynamic convolutional layer '''
+    	    ### complete the forward path --------------------
+        	#
+        	#
+        # out1=self.resnet18(imgs)
+        # out2=self.embeds(labels)
+        # out2=self.Weights_Generator_MLP(out2)
+        # print(self.DC.weight.shape)
+        # self.DC.weight=nn.Parameter(out2.expand(-1,64,-1,-1))
+        # print(self.DC.weight.shape)
+        # out1=self.DC(out1)
+        # out1=out1.view(-1,64)
+        # cls_scores=self.Class_Score_Predictor_MLP(out1)
+
+        # ### ----------------------------------------------
+        # return cls_scores # Dim: [batch_size, 10]
+        
+        cls_scores = torch.tensor((), dtype=torch.double, device=self.device)
+        cls_scores = cls_scores.new_zeros((args.batch_size, 10))
+        for i in range(10):
+            v = self.resnet18(imgs)
+            w = self.Weights_Generator_MLP(v.flatten())
+            w = F.normalize(w, p=2, dim=0)
+            w = w.view_as(self.dc.weight.data)
+            self.dc.weight.data = w
+            v_hat = self.dc(v)
+            v_hat = v_hat / np.sqrt(v_hat.size()[0])
+            re_v_hat = v_hat.view(v_hat.size(0), 64)
+            cls_scores[:, [i]] = self.w_cls(re_v_hat).double()
+    	    #
+        	#
+        	### ----------------------------------------------
+
+ 		else:
+			''' without dynamic convolutional layer '''
+    	    ### complete the forward path --------------------
+        	#
+            out1=self.resnet18(imgs)
+            out1=self.dc(out1)
+            out1=out1.view(-1,64)
+            cls_scores=self.w_cls(out1)
+       	    #
+	        #
+     	    ### ----------------------------------------------		
+		
+        return cls_scores
---
+++ train.py
@@ -0,0 +1,96 @@
+import os
+import time
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+criterion = torch.nn.CrossEntropyLoss()
+
+def train(args, model, optimizer, dataloaders):
+    trainloader, testloader = dataloaders
+
+    best_testing_accuracy = 0.0
+
+    # training
+    for epoch in range(args.epochs):
+        model.train()
+
+        batch_time = time.time(); iter_time = time.time()
+        for i, data in enumerate(trainloader):
+
+            imgs, labels = data
+            imgs, labels = imgs.to(device), labels.to(device)
+
+            cls_scores = model(imgs, with_dyn=args.with_dyn)
+            loss = criterion(cls_scores, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if i % 100 == 0 and i != 0:
+                print('epoch:{}, iter:{}, time:{:.2f}, loss:{:.5f}'.format(epoch, i,
+                    time.time()-iter_time, loss.item()))
+                iter_time = time.time()
+        batch_time = time.time() - batch_time
+        print('[epoch {} | time:{:.2f} | loss:{:.5f}]'.format(epoch, batch_time, loss.item()))
+        print('-------------------------------------------------')
+
+        if epoch % 1 == 0:
+            testing_accuracy = evaluate(args, model, testloader)
+            print('testing accuracy: {:.3f}'.format(testing_accuracy))
+
+            if testing_accuracy > best_testing_accuracy:
+                ### compare the previous best testing accuracy and the new testing accuracy
+                ### save the model and the optimizer --------------------------------
+                #
+                #
+                print('Saving....')
+                state = {
+                    'state_dict': model.state_dict(),
+                    'acc': testing_accuracy,
+                    'optimizer': optimizer.state_dict(),
+                    'epoch': epoch,}
+                torch.save(state,'model_checkpoint.pth')
+                best_testing_accuracy=testing_accuracy
+                #
+                #
+                ### -----------------------------------------------------------------
+                print('new best model saved at epoch: {}'.format(epoch))
+    print('-------------------------------------------------')
+    print('best testing accuracy achieved: {:.3f}'.format(best_testing_accuracy))
+
+def evaluate(args, model, testloader):
+    total_count = torch.tensor([0.0]); correct_count = torch.tensor([0.0])
+    for i, data in enumerate(testloader):
+        imgs, labels = data
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        total_count += labels.size(0)
+
+        with torch.no_grad():
+            cls_scores = model(imgs, with_dyn=args.with_dyn)
+
+            predict = torch.argmax(cls_scores, dim=1)
+            correct_count += (predict == labels).sum()
+    testing_accuracy = correct_count / total_count
+    return testing_accuracy.item()
+
+
+def resume(args, model, optimizer):
+    checkpoint_path = './{}_checkpoint.pth'.format(args.exp_id)
+    assert os.path.exists(checkpoint_path), ('checkpoint do not exits for %s' % checkpoint_path)
+
+    ### load the model and the optimizer --------------------------------
+    #
+    #
+    model_CKPT = torch.load(checkpoint_path)
+    model.load_state_dict(model_CKPT['state_dict'])
+    optimizer.load_state_dict(model_CKPT['optimizer'])
+    #
+    #
+    ### -----------------------------------------------------------------
+
+    print('Resume completed for the model\n')
+
+    return model, optimizer
