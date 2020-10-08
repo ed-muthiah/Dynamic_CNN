392c9f7e817ac10710ec63ba737a5a9eb0c8d42e
Ed Muthiah <ed.muthiah@gmail.com>
2020-10-09 04:54:35 +1100

    Update model.py

--- model.py
+++ model.py
@@ -41,43 +41,45 @@ class BaseModel(nn.Module):
 
     def forward(self, imgs,with_dyn=True):
         if with_dyn:
-            ''' with dynamic convolutional layer '''        
-        cls_scores = torch.tensor((), dtype=torch.double, device=self.device)
-        cls_scores = cls_scores.new_zeros((64, 10)) #64 is batch size
-        v = self.dcn_model(imgs)
-        #print('imgs size is', imgs.size())
-        #print('v size is', v.size())
-#imgs size is torch.Size([64, 3, 32, 32])
-#v size is torch.Size([64, 64, 8, 8])
-#w size is torch.Size([64, 576])
-#self.dc[0].weight.data size is torch.Size([1, 64, 3, 3])
+            ''' with dynamic convolutional layer '''
+            print('with dynamic convolutional layer')
+            cls_scores = torch.tensor((), dtype=torch.double, device=self.device)
+            cls_scores = cls_scores.new_zeros((64, 10)) #64 is batch size
+            v = self.dcn_model(imgs)
+            #print('imgs size is', imgs.size())
+            #print('v size is', v.size())
+    #imgs size is torch.Size([64, 3, 32, 32])
+    #v size is torch.Size([64, 64, 8, 8])
+    #w size is torch.Size([64, 576])
+    #self.dc[0].weight.data size is torch.Size([1, 64, 3, 3])
 
-        w = self.w_dyn(v.view(imgs.size(0),-1))
-        w_normalised = F.normalize(w, p=2, dim=0)
-        #print('w size is', w.size())
-        for img in range(v.size(0)):
-            #print('starting forward')
-            #print('self.dc[0].weight.data size is', self.dc[0].weight.data.size())
-            w_img = w_normalised[img,:].view_as(self.dc[0].weight.data)
-            with torch.no_grad():
-                self.dc[0].weight.data = w_img
-            
-            v_i = torch.unsqueeze(v[img,:, :, :], 0)
-            #print('v_i', v_i.size())
-            
-            v_hat_i = self.dc(v_i)
-            v_hat_i = v_hat_i / torch.norm(v_hat_i)
-            v_hat_i = v_hat_i.view(v_hat_i.size(0), 64)
-            cls_scores[[img],:] = self.w_cls(v_hat_i).double()
-            #print('done wooohooo')
-            #sys.exit()
-            
-            #
-            #
-            ### ----------------------------------------------
-            
+            w = self.w_dyn(v.view(imgs.size(0),-1))
+            w_normalised = F.normalize(w, p=2, dim=0)
+            #print('w size is', w.size())
+            for img in range(v.size(0)):
+                #print('starting forward')
+                #print('self.dc[0].weight.data size is', self.dc[0].weight.data.size())
+                w_img = w_normalised[img,:].view_as(self.dc[0].weight.data)
+                with torch.no_grad():
+                    self.dc[0].weight.data = w_img
+
+                v_i = torch.unsqueeze(v[img,:, :, :], 0)
+                #print('v_i', v_i.size())
+
+                v_hat_i = self.dc(v_i)
+                v_hat_i = v_hat_i / torch.norm(v_hat_i)
+                v_hat_i = v_hat_i.view(v_hat_i.size(0), 64)
+                cls_scores[[img],:] = self.w_cls(v_hat_i).double()
+                #print('done wooohooo')
+                #sys.exit()
+
+                #
+                #
+                ### ----------------------------------------------
+
         else:
             ''' without dynamic convolutional layer '''
+            print('withOUT dynamic convolutional layer')
             out1=self.dcn_model(imgs)
             out1=self.dc(out1)
             out1=out1.view(-1,64)
