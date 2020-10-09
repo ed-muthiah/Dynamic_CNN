cc194c262628305f438828edcde386a4a0562212
Ed Muthiah <ed.muthiah@gmail.com>
2020-10-09 17:03:14 +1100

    Update train.py

--- train.py
+++ train.py
@@ -12,23 +12,26 @@ def train(args, model, optimizer, dataloaders):
     trainloader, testloader = dataloaders
 
     best_testing_accuracy = 0.0
-    train_loss=[]
+    train_loss=[] # this we have
+    train_accuracies=[] # this we find by modifying the train code
     epoch_x=[]
-    test_accuracies=[]
-    train_accuracies=[]
+    test_accuracies=[] # this we have
+    test_losses = [] # this we find by modifying the eval code
+    
     # training
     for epoch in range(args.epochs):
-        running_loss = 0.0
-        #model.train()
+        total_train_count = torch.tensor([0.0]).cuda(); correct_train_count = torch.tensor([0.0]).cuda()
+        model.train()
         batch_time = time.time(); iter_time = time.time()
         for i, data in enumerate(trainloader):
-
             imgs, labels = data
             imgs, labels = imgs.to(device), labels.to(device)
-
+            total_train_count += labels.size(0)
             cls_scores = model(imgs, with_dyn=args.with_dyn)
             loss = criterion(cls_scores, labels)
-
+            predict = torch.argmax(cls_scores, dim=1)
+            correct_train_count += (predict == labels).sum()
+            train_accuracy = correct_count / total_count
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -41,11 +44,13 @@ def train(args, model, optimizer, dataloaders):
         print('[epoch {} | time:{:.2f} | loss:{:.5f}]'.format(epoch, batch_time, loss.item()))
         print('-------------------------------------------------')
         train_loss.append(loss.item())
+        train_loss.append(train_accuracy.item())
         epoch_x.append(epoch)
 
         if epoch % 1 == 0:
             testing_accuracy,testing_loss = evaluate(args, model, testloader)
             test_accuracies.append(testing_accuracy)
+            test_losses.append(testing_loss)
             print('testing accuracy: {:.3f}'.format(testing_accuracy))
 
             if testing_accuracy > best_testing_accuracy:
@@ -72,30 +77,30 @@ def train(args, model, optimizer, dataloaders):
     fig, axs = plt.subplots(2, 2)
     axs[0, 0].plot(epoch_x, train_loss)
     axs[0, 0].set_title('Epoch vs. Training Loss')
-    #axs[0, 1].plot(epoch_x, y)
-    #axs[0, 1].set_title('Epoch vs. Training Accuracy')
-    #axs[1, 0].plot(epoch_x, y)
-    #axs[1, 0].set_title('Epoch vs. Test Loss')
+    axs[0, 1].plot(epoch_x, y)
+    axs[0, 1].set_title('Epoch vs. Training Accuracy')
+    axs[1, 0].plot(epoch_x, y)
+    axs[1, 0].set_title('Epoch vs. Test Loss')
     axs[0, 1].plot(epoch_x, test_accuracies)
     axs[0, 1].set_title('Epoch vs. Test Accuracy')
     plt.savefig('with_dyn.jpg')
     
 def evaluate(args, model, testloader):
+    model.eval()
     total_count = torch.tensor([0.0]).cuda(); correct_count = torch.tensor([0.0]).cuda()
-    running_loss = torch.tensor([0.0]).cuda()
+    #running_loss = torch.tensor([0.0]).cuda()
     for i, data in enumerate(testloader):
         imgs, labels = data
         imgs, labels = imgs.to(device), labels.to(device)
 
         total_count += labels.size(0)
-
         with torch.no_grad():
             cls_scores = model(imgs, with_dyn=args.with_dyn)
-            loss = criterion(cls_scores, labels.unsqueeze(1).float())
-            running_loss += loss.item()*imgs.size(0)
+            loss = criterion(cls_scores, labels)
+            #running_loss += loss.item()*imgs.size(0)
             predict = torch.argmax(cls_scores, dim=1)
             correct_count += (predict == labels).sum()
-    testing_loss = running_loss/len(testloader.dataset)        
+    testing_loss = loss.item()
     testing_accuracy = correct_count / total_count
     return testing_accuracy.item(),testing_loss
 
