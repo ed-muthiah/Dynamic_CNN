4791cbbe6ed781b6b2a513fc1993b908d274ce4b
Ed Muthiah <ed.muthiah@gmail.com>
2020-10-10 15:27:45 +1100

    Update model.py

--- model.py
+++ model.py
@@ -22,16 +22,24 @@ class BaseModel(nn.Module):
         ### initialize the BaseModel --------------------------------
         #
         #
-        self.resnet18 = models.resnet18(pretrained=True)
-        # self.resnet18 = self.net.cuda() if self.device else self.net
-        self.dcn_model = nn.Sequential(*list(self.resnet18.children())[:5])
+        # Get full ResNet18 model
+        self.resnet18 = models.resnet18(pretrained=True) 
+        
+        # Initialize our custom DCN model as the first 5 layers of ResNet up to BasicBlock (1)
+        self.dcn_model = nn.Sequential(*list(self.resnet18.children())[:5]) 
+        
+        # Initialize the Weight Generator network
         self.w_dyn = nn.Sequential(
             nn.Linear(4096,576),
             nn.Tanh()
             )
+        
+        # Initialize the Dynamic Convolutional (DC) layer
         self.dc = nn.Sequential(
             nn.Conv2d(64,1,3,stride=1,padding=1)
             )
+        
+        # Initialize the weight generator network
         self.w_cls = nn.Sequential(
             nn.Linear(64,10),
             )
@@ -42,55 +50,69 @@ class BaseModel(nn.Module):
     def forward(self, imgs,with_dyn=True):
         if with_dyn:
             ''' with dynamic convolutional layer '''
-            #print('with dynamic convolutional layer')
+            # Notes for debugging
+            # Batch Size was 64
+            # imgs size was torch.Size([64, 3, 32, 32])
+            # v size was torch.Size([64, 64, 8, 8])
+            # w size was torch.Size([64, 576])
+            # self.dc[0].weight.data size was torch.Size([1, 64, 3, 3])
+            
+            # Passing input imgs through backbone to get feature map v
             v = self.dcn_model(imgs)
-#            print('imgs size is', imgs.size())
-#            print('v size is', v.size())
-    #imgs size is torch.Size([64, 3, 32, 32])
-    #v size is torch.Size([64, 64, 8, 8])
-    #w size is torch.Size([64, 576])
-    #self.dc[0].weight.data size is torch.Size([1, 64, 3, 3])
-
+            
+            # Flatten feature map v and feed it to weights generator network
             w = self.w_dyn(v.view(imgs.size(0),-1))
+            
+            # L2 Normalise w along rows 
             w_normalised = F.normalize(w, p=2, dim=0)
-            #print('w size is', w.size())
+            
+            # Initialize class scores tensor on GPU
             cls_scores = torch.tensor((), dtype=torch.double, device=self.device)
-            cls_scores = cls_scores.new_zeros((imgs.size(0), 10)) #64 is batch size
+            
+            # Intialize class scores matrix shape Batch Size x No. Classes
+            cls_scores = cls_scores.new_zeros((imgs.size(0), 10)) 
 
+            # Iterate through all images in batch
             for img in range(imgs.size(0)):
-                #print('starting forward')
-                #print('self.dc[0].weight.data size is', self.dc[0].weight.data.size())
-
+                # Get the normalised dynamic network weights for each image and reshape
                 w_img = w_normalised[img,:].view_as(self.dc[0].weight.data)
-                #print('w img size is', w_img.size())
+
                 with torch.no_grad():
+                    # Set the weights of DC layer 
                     self.dc[0].weight.data = w_img
 
+                # Unsqueeze to make 3D into 4D for image feature map
                 v_i = torch.unsqueeze(v[img,:, :, :], 0)
-                #print('v_i', v_i.size())
-
+                
+                # Feed feature map v_i through the DC layer
                 v_hat_i = self.dc(v_i)
-                #print('v_hat_i', v_hat_i.size())
-                #print('v_hat_i(0)', v_hat_i.size(0))
+                
+                # Normalise the image representation v_hat_i
                 v_hat_i = v_hat_i / torch.norm(v_hat_i)
-                #print('imgs size 0 is', imgs.size(0))
-                v_hat_i = v_hat_i.view(v_hat_i.size(0), imgs.size(0))                
+                
+                # Reshape
+                v_hat_i = v_hat_i.view(v_hat_i.size(0), imgs.size(0))         
+                
+                # Pass v_hat_i to the classification layer to get all 10 class scores
                 cls_scores[[img],:] = self.w_cls(v_hat_i).double()
-                #print('done wooohooo')
-                #sys.exit()
-
                 #
                 #
                 ### ----------------------------------------------
 
         else:
             ''' without dynamic convolutional layer '''
-            #print('withOUT dynamic convolutional layer')
-            out1=self.dcn_model(imgs)
-            out1=self.dc(out1)
-            out1=out1.view(-1,64)
-            cls_scores=self.w_cls(out1)
+            # To compare training stats to the same network without dynamic filters, 
+            # we stop using the w_dyn network. This means that the network is simple
+            # and only has 3 layers, DCN_model (from ResNet18) > DC layer > Classfication Layer
             
+            # Pass imgs through our custom DCN Model
+            output1=self.dcn_model(imgs)
+            
+            # Pass layer 1 output through DC layer
+            output2=self.dc(output1)
+            
+            # Reshape and pass output of layer 2 through classification layer to get class scores
+            cls_scores=self.w_cls(output2.view(-1,64))
             #
             ### ---------------------------------------------
         return cls_scores
